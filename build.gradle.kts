38cf02dfde3cb1748fabbb63f0cd229c95154667
Dmitriy Ustinov <48065150+qu0d@users.noreply.github.com>
2024-05-20 18:55:05 +0300

    Initial commit (#2)
    * Initial commit
    
    * Github workflows actions and true GAV
    
    * Maven repositories
    
    * JDK 17
    
    * JDK 21
    
    * Constant settings
    
    * Fix incompatible version of Kotlin
    
    * logback, parameters full name and metarunners subproject
    
    * Downgrade logback to 1.3.14 for jvm 1.6
    
    * One jar with multiple tasks
    
    * Fix obsolete description
    
    * Plus to multiple tasks
    
    * Dependency improvement
    
    * Logback version
    
    * Dependency version in build.gradle
    
    ---------
    
    Co-authored-by: Dmitry Ustinov <dustinov.mbox@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    application
+    `maven-publish`
+}
+
+// TODO: +Settings
+group = "org.octopusden.octopus.automation"
+description = "Octopus Teamcity Automation"
+ext {
+    set("metarunnerId", "OctopusTeamcityAutomation")
+    set("mainClassPackage", "org.octopusden.octopus.automation.teamcity")
+    set("artifactId", "teamcity-automation")
+}
+
+tasks.compileKotlin {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+java.targetCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven(url = "https://packages.jetbrains.team/maven/p/teamcity-rest-client/teamcity-rest-client")
+}
+
+dependencies {
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    implementation("ch.qos.logback:logback-classic:1.3.14")
+    implementation("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.5")
+    // TODO: +Custom dependencies
+    implementation("org.jetbrains.teamcity:teamcity-rest-client:3.5")
+}
+
+application {
+    mainClass = "${project.properties["mainClassPackage"]}.ApplicationKt"
+}
+
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
+    val contents = configurations.runtimeClasspath.get().map { f ->
+        if (f.isDirectory) f else zipTree(f)
+    } + sourceSets.main.get().output
+    from(contents)
+    archiveBaseName = project.properties["artifactId"] as String
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            artifactId = project.properties["artifactId"] as String
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version ("1.9.22")
+    }
+}
+
+// TODO: +Project name
+rootProject.name = "octopus-teamcity-automation"
+
+include("teamcity-meta-runner")
