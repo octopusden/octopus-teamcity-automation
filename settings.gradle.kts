5976b2d2c4ee335a6c47c555394c853d668e7b13
Roman Ivanov <ivanov.roman.alexeevich@gmail.com>
2024-08-14 12:29:20 +0300

    Add default value for version. Upgrade kotlin from 1.8.0 to 1.9.20.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("org.jetbrains.kotlin.jvm") version ("1.8.0")
+        id("org.jetbrains.kotlin.jvm") version ("1.9.20")
         id("com.github.johnrengelman.shadow") version ("8.1.1")
         id("com.avast.gradle.docker-compose") version ("0.16.9")
         id("io.github.gradle-nexus.publish-plugin") version ("1.1.0")
