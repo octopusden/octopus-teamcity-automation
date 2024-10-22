7f5b8d42f71704d415af0ee479df40799dbd8520
adellinekania <adellinekaniasetiyawan@gmail.com>
2024-10-22 08:18:38 +0700

    add comment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,9 @@ pluginManagement {
         id("com.github.johnrengelman.shadow") version ("8.1.1")
         id("com.avast.gradle.docker-compose") version ("0.16.9")
         id("io.github.gradle-nexus.publish-plugin") version ("1.1.0")
+
+        // For publishing artifacts for testing purpose
+        // Can be removed once the FT of the TeamcityCreateBuildChainCommand implemented
         id("org.octopusden.octopus-release-management") version ("2.0.28")
     }
 }
