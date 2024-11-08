a3f1e4f3fe3bda245a74a0ef83f1996834178b0e
adellinekania <adellinekaniasetiyawan@gmail.com>
2024-11-08 14:50:43 +0700

    add octopus-release-management.version in property

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,12 @@
 pluginManagement {
+    val releaseManagementVersion = extra["octopus-release-management.version"] as String
+
     plugins {
         id("org.jetbrains.kotlin.jvm") version ("1.9.20")
         id("com.github.johnrengelman.shadow") version ("8.1.1")
         id("com.avast.gradle.docker-compose") version ("0.16.9")
         id("io.github.gradle-nexus.publish-plugin") version ("1.1.0")
-        id("org.octopusden.octopus-release-management") version ("2.0.28")
+        id("org.octopusden.octopus-release-management") version(releaseManagementVersion)
     }
 }
 
