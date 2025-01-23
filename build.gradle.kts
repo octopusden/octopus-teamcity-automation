d1fe3845d20b15e8d13f7a230f9f919a96c37c73
fishinitself <mikhail.ershov@gmail.com>
2025-01-23 16:05:14 +0200

    attempt to fix docker permissions issue in a simple way

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ tasks.register<Sync>("prepareTeamcityServerData") {
     from(zipTree(layout.projectDirectory.file("docker/data.zip")))
     into(layout.buildDirectory.dir("teamcity-server"))
     filePermissions { unix("rwxrwxrwx") }
-    dirPermissions { unix("drwxrwxrwx") }
+    dirPermissions { unix("rwxrwxrwx") }
 }
 
 tasks.named("composeUp") {
