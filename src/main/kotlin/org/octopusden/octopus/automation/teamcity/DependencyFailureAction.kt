7c3872d4ddd82a96cda97f9f0811d27eb317e7d6
Gabriella Naomi Hutagalung <gabriellanaomi37@gmail.com>
2026-03-17 16:34:24 +0700

    Delete unused enum

--- src/main/kotlin/org/octopusden/octopus/automation/teamcity/DependencyFailureAction.kt
+++ src/main/kotlin/org/octopusden/octopus/automation/teamcity/DependencyFailureAction.kt
@@ -1,6 +1,5 @@
 package org.octopusden.octopus.automation.teamcity
 
 enum class DependencyFailureAction(val value: String) {
-    MAKE_FAILED_TO_START("MAKE_FAILED_TO_START"),
     CANCEL("CANCEL")
 }
