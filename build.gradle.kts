aedea4302b2f9e5c6073751c5dc2eee29532e545
Gabriella Naomi Hutagalung <gabriellanaomi37@gmail.com>
2026-05-04 16:23:59 +0700

    Increase memory limit for TeamCity OKD pod  (#57)

--- build.gradle.kts
+++ build.gradle.kts
@@ -129,7 +129,7 @@ ocTemplate {
                 "CPU_REQUEST" to "500m",
                 "CPU_LIMIT" to "2500m",
                 "MEM_REQUEST" to "1.5Gi",
-                "MEM_LIMIT" to "2Gi"
+                "MEM_LIMIT" to "3Gi"
             ))
         }
         service("teamcity25") {
@@ -140,8 +140,8 @@ ocTemplate {
                 "TEAMCITY_ID" to "25",
                 "CPU_REQUEST" to "200m",
                 "CPU_LIMIT" to "2500m",
-                "MEM_REQUEST" to "1.2Gi",
-                "MEM_LIMIT" to "2Gi"
+                "MEM_REQUEST" to "1.5Gi",
+                "MEM_LIMIT" to "3Gi"
             ))
         }
     }
